8fd082108f5273d8e86bb30ccca4b9dc59b3d15e
bosakth <boopalan.sakthivel_m@mercedes-benz.com>
2024-04-26 14:09:38 +0530

    added fetch all response api

--- app/src/main/java/com/optifetch/ApiService.kt
+++ app/src/main/java/com/optifetch/ApiService.kt
@@ -7,4 +7,7 @@ import retrofit2.http.Path
 interface ApiService {
     @GET("posts/{id}")
     fun getPostById(@Path("id") postId: Int): Call<Post>
+
+    @GET("posts")
+    fun getPost(): Call<List<Post>>
 }
--- app/src/main/java/com/optifetch/MainActivity.kt
+++ app/src/main/java/com/optifetch/MainActivity.kt
@@ -5,6 +5,8 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -14,6 +16,39 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        button1Actions()
+        button2Actions()
+    }
+
+    private fun button2Actions() {
+        val button = findViewById<Button>(R.id.btn2)
+        val tv = findViewById<TextView>(R.id.textView)
+        button.setOnClickListener {
+            val call = ApiClient.apiService.getPost()
+
+            call.enqueue(object : Callback<List<Post>> {
+                override fun onResponse(call: Call<List<Post>>, response: Response<List<Post>>) {
+                    if (response.isSuccessful) {
+                        val post = response.body()
+                        tv.text = post.toString()
+                    } else {
+                        // Handle error
+                    }
+                }
+
+                override fun onFailure(call: Call<List<Post>>, t: Throwable) {
+                    // Handle failure
+                }
+            })
+        }
+    }
+
+    private fun button1Actions() {
         val button = findViewById<Button>(R.id.btn)
         val tv = findViewById<TextView>(R.id.textView)
         button.setOnClickListener {
